a49313a86c006526280edccd01af8adb60dd2ef6
ZannKrus <damiryusupov2504@gmail.com>
2025-04-01 09:28:24 +0500

    FastApi

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, HTTPException, Query
+import random
+import math
+
+app = FastAPI()
+
+@app.get("/about")
+async def about():
+    # Возвращаем информацию о разработчике API
+    return {
+        "ФИО": "Иванов Иван Иванович",
+        "Группа": "Группа 101",
+        "Возраст": "21 год"
+    }
+
+@app.get("/rnd")
+async def get_random():
+    # Возвращаем случайное число от 1 до 10
+    return {"Случайное_число": random.randint(1, 10)}
+
+@app.post("/t_square")
+async def triangle(
+    a: float = Query(..., gt=0, description="Длина стороны a (больше 0)"),
+    b: float = Query(..., gt=0, description="Длина стороны b (больше 0)"),
+    c: float = Query(..., gt=0, description="Длина стороны c (больше 0)")
+):
+    if (a + b <= c) or (a + c <= b) or (b + c <= a):
+        raise HTTPException(status_code=400, detail="Треугольник с такими сторонами не может существовать.")
+    
+    perimeter = a + b + c
+    
+    s = perimeter / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    
+    return {"Периметр": perimeter, "Площадь": area}
